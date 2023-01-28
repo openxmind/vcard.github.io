e9703c7d534e3ae5ac189fac13a5cef625df1991
openxmind <openxmind1@proton.me>
2023-01-28 01:31:26 +0100

    added new files

---
+++ app/Http/Requests/UpdateSettingRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Setting;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateSettingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = Setting::$rules;
+        $rules['email'] = 'required|email:filter';
+        $rules['favicon'] = 'nullable|mimes:jpg,jpeg,png';
+        $rules['address'] = 'required';
+        $rules['home_page_banner'] = 'nullable|mimes:jpg,jpeg,png';
+
+        return $rules;
+    }
+}
